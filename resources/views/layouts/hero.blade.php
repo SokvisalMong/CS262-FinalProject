1dcaf4567b2eeb153316d8beec579b9b410b71e1
Sivtheng <sivtheng25@gmail.com>
2022-10-14 23:05:08 +0700

    tailwind

---
+++ resources/views/layouts/hero.blade.php
@@ -0,0 +1,20 @@
+{{-- Hero section --}}
+<div class="flex-col text-white bg-gray-500 py-10">
+    {{-- big text --}}
+    <div class="font-bold text-center text-6xl py-5"> 
+        Find your table for any occasion
+    </div>
+    {{-- the second part --}}
+    <div class="text-center">
+        {{-- text --}}
+        <div class="text-2xl">
+            View all restaurants in Phnom Penh
+        </div>
+        {{-- let's go button --}}
+        <div class="py-5">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white py-4 px-8 rounded-lg">
+                <a href="">Let's go</a>
+            </button>
+        </div>
+    </div>
+</div>
