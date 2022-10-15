14404a94f99547e1ba5ffca73b7b356b1f52bd5c
SokvisalMong <smong@paragoniu.edu.kh>
2022-10-16 06:04:08 +0700

    Removed the 100 character limit on password varchar

--- database/migrations/2022_10_15_140832_create_users_table.php
+++ database/migrations/2022_10_15_140832_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->integer('user_id')->autoIncrement();
 
             $table->string('user_email', 100);
-            $table->string('user_password', 100);
+            $table->string('user_password');
             $table->string('user_firstname', 100);
             $table->string('user_lastname', 100);
             $table->string('user_mobile', 100);
