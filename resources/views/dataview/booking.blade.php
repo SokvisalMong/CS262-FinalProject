dc45a9e908f8340aceb10ada64a1afd3141de75a
SokvisalMong <smong@paragoniu.edu.kh>
2022-10-16 05:22:18 +0700

    Forgot to remove the bootstrap css from booking.blade lol

--- resources/views/dataview/booking.blade.php
+++ resources/views/dataview/booking.blade.php
@@ -6,11 +6,11 @@
     <table class="table-auto">
         <thead>
             <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Date</th>
-                <th scope="col">Time</th>
-                <th scope="col">Size</th>
-                <th scope="col">Status</th>
+                <th>ID</th>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Size</th>
+                <th>Status</th>
             </tr>
         </thead>
         <tbody>
@@ -23,11 +23,11 @@
             </tr>
             @foreach ($v_bookings as $item)
                 <tr>
-                    <td scope="row">{{$item->booking_id}}</td>
-                    <td scope="row">{{$item->booking_date}}</td>
-                    <td scope="row">{{$item->booking_time}}</td>
-                    <td scope="row">{{$item->party_size}}</td>
-                    <td scope="row">{{$item->booking_status}}</td>
+                    <td>{{$item->booking_id}}</td>
+                    <td>{{$item->booking_date}}</td>
+                    <td>{{$item->booking_time}}</td>
+                    <td>{{$item->party_size}}</td>
+                    <td>{{$item->booking_status}}</td>
                 </tr>    
             @endforeach
         </tbody>
