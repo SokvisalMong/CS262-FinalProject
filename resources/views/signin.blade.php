c9ec7bf2847e8cfa528470c6152dc4820ab564b8
Sivtheng <sivtheng25@gmail.com>
2022-10-15 09:13:56 +0700

    make sign in form in the middle

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('content')
-<div class="w-full max-w-xs">
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+<div class=" max-w-xs mx-auto mt-48">
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 ">
       <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
           Username
@@ -12,8 +12,7 @@
         <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
           Password
         </label>
-        <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
-        <p class="text-red-500 text-xs italic">Please choose a password.</p>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
       </div>
       <div class="flex items-center justify-between">
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
