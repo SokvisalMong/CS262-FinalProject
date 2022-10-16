29dba1c0cd1c3241b0e0474c897574e23850e3b6
Sivtheng <sivtheng25@gmail.com>
2022-10-16 09:50:00 +0700

    fixed routing all lowercase

--- routes/web.php
+++ routes/web.php
@@ -24,18 +24,18 @@ Route::get('/', [PagesController::class, 'Home']);
 Route::get('/dashboard', [PagesController::class, 'Dashboard']); // route for dashboard needs to be lower case cus we dont have button to click into dashboard
                                                                  // we cant type Uppercase in the url for some whatever reason
 
-Route::get('/RestaurantList', [PagesController::class, 'RestaurantList']);
+Route::get('/restaurantlist', [PagesController::class, 'RestaurantList']);
 
-Route::get('/Register', [PagesController::class, 'Register']);
+Route::get('/register', [PagesController::class, 'Register']);
 
-Route::get('/SignIn', [PagesController::class, 'SignIn']);
+Route::get('/signin', [PagesController::class, 'SignIn']);
 
 
 
-Route::get('/Show/Restaurant', [RestaurantController::class, 'ShowRestaurant']);
+Route::get('/show/restaurant', [RestaurantController::class, 'ShowRestaurant']);
 
-Route::get('/Show/User', [UserController::class, 'ShowUser']);
+Route::get('/show/user', [UserController::class, 'ShowUser']);
 
-Route::get('/Show/Booking', [BookingController::class, 'ShowBooking']);
+Route::get('/show/booking', [BookingController::class, 'ShowBooking']);
 
-Route::get('/Show/Admin', [AdminController::class, 'ShowAdmin']);
+Route::get('/show/admin', [AdminController::class, 'ShowAdmin']);
