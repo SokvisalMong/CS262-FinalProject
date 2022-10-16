cabcd7cd9677a376d138cc111be7f81d836302e5
Sivtheng <sivtheng25@gmail.com>
2022-10-16 18:40:55 +0700

    edited typo in previous commit

--- resources/views/dashboard/dashsidebar.blade.php
+++ resources/views/dashboard/dashsidebar.blade.php
@@ -38,7 +38,7 @@
       </li>
       <li class="relative">
         <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out" 
-        href="/dashboard/bookingform" >
+        href="/dashboard/restaurantform" >
             Restaurant Form
         </a>
       </li>
@@ -50,7 +50,7 @@
       </li>
       <li class="relative">
         <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out" 
-        href="/dashboard/bookingform" >
+        href="/dashboard/userform" >
             User Form
         </a>
       </li>
