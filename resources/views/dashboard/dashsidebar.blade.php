e37e606270cc4cb11e1c2b5b89d28a7271e51d1c
Sivtheng <sivtheng25@gmail.com>
2022-10-16 18:40:13 +0700

    changed the order of the sidebar links

--- resources/views/dashboard/dashsidebar.blade.php
+++ resources/views/dashboard/dashsidebar.blade.php
@@ -11,15 +11,35 @@
         href="/dashboard/admintable" >
             Admin Table
         </a>
+      </li>
+      <li class="relative">
+        <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out" 
+        href="/dashboard/adminform" >
+            Admin Form
+        </a>
+      </li>
       <li class="relative">
         <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out" 
         href="/dashboard/bookingtable" >
             Booking Table
         </a>
+      </li>
+      <li class="relative">
+        <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out" 
+        href="/dashboard/bookingform" >
+            Booking Form
+        </a>
+      </li>
       <li class="relative">
         <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out" 
         href="/dashboard/restauranttable" >
             Restaurant Table
+        </a>
+      </li>
+      <li class="relative">
+        <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out" 
+        href="/dashboard/bookingform" >
+            Restaurant Form
         </a>
       </li>
       <li class="relative">
@@ -30,16 +50,8 @@
       </li>
       <li class="relative">
         <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out" 
-        href="/dashboard/adminform" >
-            Admin Form
-        </a>
-      </li>
-      <li class="relative">
-        <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out" 
-
         href="/dashboard/bookingform" >
-            Booking Form
-
+            User Form
         </a>
       </li>
     </ul>
