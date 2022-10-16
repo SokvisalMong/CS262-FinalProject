6ee48ac71804aad663c9875ed01011a95aba1ead
Thyyy1 <rythyy012@gmail.com>
2022-10-16 14:26:04 +0700

    edited adminform

--- resources/views/dashboard/adminform.blade.php
+++ resources/views/dashboard/adminform.blade.php
@@ -3,21 +3,24 @@
 @section('content')
 <div class="w-full max-ws">
     <form method="POST" action="/AddAdmin" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        @csrf
         <div class="mb-6">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
                 Username
             </label>
-            <input class="shadow border border-gray-200 rounded p-2 w-full">
+            <input type="text" name="admin_username" class="shadow border border-gray-200 rounded p-2 w-full">
         </div>
         <div class="mb-6">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
                 Email
             </label>
+            <input type="email" name="admin_email" class="shadow border border-gray-200 rounded p-2 w-full">
         </div>
         <div class="mb-6">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                 Password
             </label>
+            <input type="password" name="admin_password" class="shadow border border-gray-200 rounded p-2 w-full">
         </div>
         <div class="md:flex md:items-center">
             <button class="bg-gray-800 text-white rounded py-2 px-4 hover:bg-black">
