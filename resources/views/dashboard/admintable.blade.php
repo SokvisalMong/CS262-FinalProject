2b148dffa22688ba3ad3eeb619995f469905750b
Sivtheng <sivtheng25@gmail.com>
2022-10-16 12:22:55 +0700

    fixed admintable

--- resources/views/dashboard/admintable.blade.php
+++ resources/views/dashboard/admintable.blade.php
@@ -33,7 +33,7 @@
 
 
 
-<div class="overflow-x-auto relative">
+<div class="mx-20 mt-14">
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -51,43 +51,23 @@
         <tbody>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                 <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
+                    Example ID
                 </th>
                 <td class="py-4 px-6">
-                    Sliver
+                    Example Admin
                 </td>
                 <td class="py-4 px-6">
-                    Laptop
+                    Example Email
                 </td>
             </tr>
+            @foreach ($v_admins as $item)
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Microsoft Surface Pro
-                </th>
-                <td class="py-4 px-6">
-                    White
-                </td>
-                <td class="py-4 px-6">
-                    Laptop PC
-                </td>
-                <td class="py-4 px-6">
-                    $1999
-                </td>
+                <td scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{$item->admin_id}}</td>
+                <td class="py-4 px-6">{{$item->admin_username}}</td>
+                <td class="py-4 px-6">{{$item->admin_email}}</td>
+                <td class="py-4 px-6">{{$item->admin_password}}</td>
             </tr>
-            <tr class="bg-white dark:bg-gray-800">
-                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Magic Mouse 2
-                </th>
-                <td class="py-4 px-6">
-                    Black
-                </td>
-                <td class="py-4 px-6">
-                    Accessories
-                </td>
-                <td class="py-4 px-6">
-                    $99
-                </td>
-            </tr>
+            @endforeach
         </tbody>
     </table>
 </div>
