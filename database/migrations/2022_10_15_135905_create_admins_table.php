247de6c058e968ae3dfff4e1bf3b890849093a84
SokvisalMong <smong@paragoniu.edu.kh>
2022-10-17 19:17:06 +0700

    added a default root user to the admin table

--- database/migrations/2022_10_15_135905_create_admins_table.php
+++ database/migrations/2022_10_15_135905_create_admins_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -24,6 +25,10 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
+
+        DB::table('admins')->insert([
+            ['admin_username' => 'root', 'admin_email' => 'admin@email.com', 'admin_password' => bcrypt('password')]
+        ]);
     }
 
 
