8568563805287045cd034ce5e901eff709270367
SokvisalMong <smong@paragoniu.edu.kh>
2022-10-17 17:10:45 +0700

    Fixed Foreign Key issue, reason: We're not doing things the conventional way so it fucks up alot

--- database/migrations/2022_10_15_134506_create_bookings_table.php
+++ database/migrations/2022_10_15_134506_create_bookings_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->integer('booking_id')->autoIncrement();
-            $table->foreignId('restaurant_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->integer('restaurant_id');
+            $table->foreign('restaurant_id')->references('restaurant_id')->on('restaurants');
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('user_id')->on('users');
 
             $table->enum('booking_status', ['Active', 'Canceled', 'Completed']);
             $table->date('booking_date');
