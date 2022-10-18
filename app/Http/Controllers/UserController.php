cc06465e03c3aeb6dc63ab4f1002a7dc2f84e6da
SokvisalMong <smong@paragoniu.edu.kh>
2022-10-19 03:18:57 +0700

    Created a signup function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -17,5 +19,23 @@ class UserController extends Controller
 
     public function register() {
         return view('user.register');
+    }
+
+    // Creates a user account and logs them in immediately
+    public function store(Request $request) {
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3', Rule::unique('users', 'name')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'confirmed', 'min:8'],
+            'phone' => ['required', 'numeric', 'min:10']
+        ]);
+
+        $formFields['password'] = bcrypt($formFields['password']);
+
+        $user = User::create($formFields);
+
+        auth()->login($user);
+
+        return redirect('/')->with('message', 'User created and logged in.');
     }
 }
