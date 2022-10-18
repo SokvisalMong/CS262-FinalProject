5b43cd6f75074720c6a7a0e2deec7d0f2c47c67a
SokvisalMong <smong@paragoniu.edu.kh>
2022-10-19 02:29:19 +0700

    Finished bookings table migration

--- database/migrations/2022_10_18_184317_create_admins_table.php
+++ database/migrations/2022_10_18_184317_create_admins_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
 
-            $table->tiemstamp('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2022_10_18_184332_create_bookings_table.php
+++ database/migrations/2022_10_18_184332_create_bookings_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
+
+            // Foreign Keys
+            $table->foreignId('restaurant_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+            // Required
+            $table->enum('status', ['Active', 'Canceled', 'Completed']);
+            $table->date('date');
+            $table->time('time');
+            $table->integer('size');
+
             $table->timestamps();
         });
     }
