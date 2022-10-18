8c34a833ae120918e84ae9d4f8754cb90eb46f8e
SokvisalMong <smong@paragoniu.edu.kh>
2022-10-18 17:15:13 +0700

    Added a register function for owners

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -28,4 +28,20 @@ class OwnerController extends Controller
     public function Restaurant() {
         return view('owner.restaurant');
     }
+
+    public function AddOwner(Request $request) {
+        $formFields = $request->validate([
+            'owner_email' => ['required', 'email', Rule::unique('owners', 'owner_email')],
+            'owner_mobile' => 'required',
+            'owner_password' => 'required',
+        ]);
+
+        $formFields['owner_password'] = bcrypt($formFields['owner_password']);
+
+        $owner = Owner::create($formFields);
+
+        auth('owner')->login($owner);
+
+        return redirect('/dashboard')->with('message', 'Owner has been created and logged in.');
+    }
 }
