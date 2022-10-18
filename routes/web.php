2ed18ac6e46e73fc1019dd73272270d7b688a742
SokvisalMong <smong@paragoniu.edu.kh>
2022-10-19 03:39:26 +0700

    Added middleware to routes on web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\OwnerController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -24,7 +25,7 @@ Route::domain(env('APP_URL'))->group(function() {
 Route::domain('www.' .env('APP_URL'))->group(function () {
     Route::get('/', [UserController::class, 'home']);
 
-    Route::get('/login', [UserController::class, 'login']);
+    Route::get('/login', [UserController::class, 'login'])->name('login');
     
     Route::get('/register', [UserController::class, 'register']);
 });
@@ -32,9 +33,29 @@ Route::domain('www.' .env('APP_URL'))->group(function () {
 // Owner pages
 Route::domain('owner.' .env('APP_URL'))->group(function () {
     Route::get('/', [OwnerController::class, 'home']);
+
+    Route::middleware(['guest:owner'])->group(function() {  
+        Route::get('/login', [OwnerController::class, 'login'])->('owner.login');
+        
+        Route::get('/register', [OwnerController::class, 'register']);
+    });
+
+    Route::middleware(['auth:owner'])->group(function() {
+        Route::get('/dashboard', [OwnerController::class, 'dashboard']);
+
+        Route::get('/restaurant/edit', [OwnerController::class, 'restaurantEdit']);
+    });
 });
 
 // Admin pages
 Route::domain('admin.' .env('APP_URL'))->group(function() {
     Route::get('/', [AdminController::class, 'home']);
+
+    Route::middleware(['guest:admin'])->group(function() {
+        Route::get('/login', [AdminController::class, 'login'])->name('admin.login');
+    });
+
+    Route::middleware(['auth:admin'])->group(function() {
+        Route::get('/register', [AdminController::class, 'register']);
+    });
 });
