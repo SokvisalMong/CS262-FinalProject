c86bfd9392e3ba5607c38bb61bf0984099e2874a
SokvisalMong <smong@paragoniu.edu.kh>
2022-10-19 02:22:24 +0700

    Finished restaurant table migration

--- database/migrations/2022_10_18_184325_create_restaurants_table.php
+++ database/migrations/2022_10_18_184325_create_restaurants_table.php
@@ -15,6 +15,25 @@ return new class extends Migration
     {
         Schema::create('restaurants', function (Blueprint $table) {
             $table->id();
+
+            // Foreign Keys
+            $table->foreignId('owner_id')->constrained()->onDelete('cascade');
+
+            // Required
+            $table->string('name')->unique();
+            $table->set('payment', ['Cash', 'Visa', 'Mastercard', 'ABA']);
+            $table->text('hoo');
+            $table->set('cuisines', ['Khmer', 'Western', 'Chinese', 'Vietnamese', 'International']);            
+            $table->set('dress_code', ['Casual', 'Business Casual', 'Casual Elegant', 'Formal', 'Jacket']);
+            $table->integer('price_lower');
+            $table->integer('price_higher');
+
+            // Nullable
+            $table->string('website')->unique()->nullable();
+            $table->string('phone')->unique()->nullable();
+            $table->string('email')->unqiue()->nullable();
+            $table->string('picture')->nullable();
+
             $table->timestamps();
         });
     }
