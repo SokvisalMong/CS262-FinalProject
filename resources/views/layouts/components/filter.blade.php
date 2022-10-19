025eba668bcff9db04c6ff693600be28431e32ef
Sivtheng <sivthegn25@gmail.com>
2022-10-20 06:55:37 +0700

    added radio check for categories search

--- resources/views/layouts/components/filter.blade.php
+++ resources/views/layouts/components/filter.blade.php
@@ -1,13 +1,54 @@
-<div>
-<form action="/restaurants/">
-    <div>
-        <div>
-            <label>Khmer</label>
-            <input type="radio" value=""/>
+<form action="/restaurants/" class="flex justify-end">
+    <div class="p-14 ">
+        <div >
+            <h1 class="text-bold text-pink-800 text-2xl pb-2">Cuisines</h1>
+            <div class=" w-36">
+                <div class="py-1">
+                    <input type="radio" value="Khmer"/>
+                    <label>Khmer</label>
+                </div>
+                <div class="py-1">
+                    <input type="radio" value="Western"/>
+                    <label>Western</label>
+                </div>
+                <div class="py-1">
+                    <input type="radio" value="Chinese"/>
+                    <label>Chinese</label>
+                </div>
+                <div class="py-1">
+                    <input type="radio" value="Vietnamese"/>
+                    <label>Vietnamese</label>
+                </div>
+                <div class="py-1">
+                    <input type="radio" value="International"/>
+                    <label>International</label>
+                </div>
+            </div>
         </div>
-        <div>
-            
+        <div class="pt-4 w-36">
+            <h1 class="text-bold text-pink-800 text-2xl pb-2">Dress Code</h1>
+            <div class="flex-col">
+                <div class="py-1">
+                    <input type="radio" value="Casual"/>
+                    <label>Casual</label>
+                </div>
+                <div class="py-1">
+                    <input type="radio" value="Business Casual"/>
+                    <label>Business Casual</label>
+                </div>
+                <div class="py-1">
+                    <input type="radio" value="Casual Elegant"/>
+                    <label>Casual Elegant</label>
+                </div>
+                <div class="py-1">
+                    <input type="radio" value="Formal"/>
+                    <label>Formal</label>
+                </div>
+                <div class="py-1">
+                    <input type="radio" value="Jacket"/>
+                    <label>Jacket</label>
+                </div>
+            </div>
         </div>
     </div>
 </form>
-</div>
