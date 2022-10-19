a49919c151297bb0aa9a0d254a729915b4c445db
SokvisalMong <smong@paragoniu.edu.kh>
2022-10-20 05:52:51 +0700

    ye

---
+++ resources/views/layouts/components/filter.blade.php
@@ -0,0 +1,13 @@
+<div>
+<form action="/restaurants/">
+    <div>
+        <div>
+            <label>Khmer</label>
+            <input type="radio" value=""/>
+        </div>
+        <div>
+            
+        </div>
+    </div>
+</form>
+</div>
---
+++ resources/views/layouts/components/search.blade.php
@@ -0,0 +1,12 @@
+<div>
+<form action="/restaurants/">
+    <div>
+        <input type="text" name="search" placeholder="Search"/>
+    </div>
+    <div>
+        <button type="submit">
+            Search
+        </button>
+    </div>
+</form>
+</div>
