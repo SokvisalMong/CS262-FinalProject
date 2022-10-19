fde1aedf1124b3824b09ce6ea48f63b41e8c38d8
Sivtheng <sivthegn25@gmail.com>
2022-10-20 06:18:32 +0700

    added a search function

--- resources/views/layouts/components/search.blade.php
+++ resources/views/layouts/components/search.blade.php
@@ -1,12 +1,10 @@
-<div>
-<form action="/restaurants/">
-    <div>
-        <input type="text" name="search" placeholder="Search"/>
-    </div>
-    <div>
-        <button type="submit">
-            Search
-        </button>
-    </div>
-</form>
+<div class="flex justify-center">
+    <form action="/restaurants/" class="mt-6 w-96" >
+        <div class="flex ">
+            <input type="text" name="search" placeholder="Search" class=" w-full border-2 border-grey-300 py-2 px-4 rounded"/>
+            <button type="submit" class="border-2 border-blue-300 px-4 rounded">
+                Search
+            </button>
+        </div>
+    </form>
 </div>
