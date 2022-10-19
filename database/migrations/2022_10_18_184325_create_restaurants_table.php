92cab2fcb1e16b20d602ac7379855bb1fd4c5479
SokvisalMong <smong@paragoniu.edu.kh>
2022-10-20 03:16:44 +0700

    hm

--- database/migrations/2022_10_18_184325_create_restaurants_table.php
+++ database/migrations/2022_10_18_184325_create_restaurants_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -37,6 +38,22 @@ return new class extends Migration
 
             $table->timestamps();
         });
+        
+        DB::table('restaurants')->insert([[
+            'owner_id' => 1,
+            'name' => 'restaurant',
+            'payment' => 'Cash',
+            'hoo' => '6AM - 6PM',
+            'cuisines' => 'Khmer',
+            'dress_code' => 'Casual',
+            'price_lower' => 5,
+            'price_higher' => 10,
+
+            'description' => 'text',
+            'website' => 'www.site.com',
+            'phone' => '012345678',
+            'email' => 'restaurant@email.com',
+        ]]);
     }
 
     /**
