99c86dffab8fdc4a2b79fdde93ccca6f471e186d
Sivtheng <sivthegn25@gmail.com>
2022-10-20 07:13:17 +0700

    added submit button

--- resources/views/layouts/components/filter.blade.php
+++ resources/views/layouts/components/filter.blade.php
@@ -1,26 +1,26 @@
-<form action="/restaurants/" class="flex justify-end">
-    <div class="p-14 ">
+<form action="/restaurants/">
+    <div class="p-14">
         <div >
             <h1 class="text-bold text-pink-800 text-2xl pb-2">Cuisines</h1>
             <div class=" w-36">
                 <div class="py-1">
-                    <input type="radio" value="Khmer"/>
+                    <input type="radio" value="Khmer" name="cuisines"/>
                     <label>Khmer</label>
                 </div>
                 <div class="py-1">
-                    <input type="radio" value="Western"/>
+                    <input type="radio" value="Western" name="cuisines"/>
                     <label>Western</label>
                 </div>
                 <div class="py-1">
-                    <input type="radio" value="Chinese"/>
+                    <input type="radio" value="Chinese" name="cuisines"/>
                     <label>Chinese</label>
                 </div>
                 <div class="py-1">
-                    <input type="radio" value="Vietnamese"/>
+                    <input type="radio" value="Vietnamese" name="cuisines"/>
                     <label>Vietnamese</label>
                 </div>
                 <div class="py-1">
-                    <input type="radio" value="International"/>
+                    <input type="radio" value="International" name="cuisines"/>
                     <label>International</label>
                 </div>
             </div>
@@ -29,26 +29,29 @@
             <h1 class="text-bold text-pink-800 text-2xl pb-2">Dress Code</h1>
             <div class="flex-col">
                 <div class="py-1">
-                    <input type="radio" value="Casual"/>
+                    <input type="radio" value="Casual" name="dress_code"/>
                     <label>Casual</label>
                 </div>
                 <div class="py-1">
-                    <input type="radio" value="Business Casual"/>
+                    <input type="radio" value="Business Casual" name="dress_code"/>
                     <label>Business Casual</label>
                 </div>
                 <div class="py-1">
-                    <input type="radio" value="Casual Elegant"/>
+                    <input type="radio" value="Casual Elegant" name="dress_code"/>
                     <label>Casual Elegant</label>
                 </div>
                 <div class="py-1">
-                    <input type="radio" value="Formal"/>
+                    <input type="radio" value="Formal" name="dress_code"/>
                     <label>Formal</label>
                 </div>
                 <div class="py-1">
-                    <input type="radio" value="Jacket"/>
+                    <input type="radio" value="Jacket" name="dress_code"/>
                     <label>Jacket</label>
                 </div>
             </div>
+            <button type="submit" class="border-2 border-blue-300 px-4 rounded">
+                Search
+            </button>
         </div>
     </div>
 </form>
