dd8176c3da67b38ada3b77b8de12dd6c791b19d2
vsea111 <vsea@paragoniu.edu.kh>
2022-10-20 22:00:36 +0700

    owner table styling

--- resources/views/booking/ownertable.blade.php
+++ resources/views/booking/ownertable.blade.php
@@ -5,61 +5,75 @@
 @endsection
 
 @section('content')
-<div class="mx-20 mt-14 text-white">
-    <div class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <table>
-            <thead>
-                <tr>
-                    <td>Date</td>
-                    <td>Time</td>
-                    <td>Size</td>
-                    <td>Status</td>
-                    <td>Cancel</td>
-                    <td>Complete</td>
-                </tr>
-            </thead>
-            <tbody>
-                @unless($bookings->isEmpty())
-                @foreach($bookings as $booking)
-                <tr>
-                    <td></td>
-                    <td>{{$booking->date}}</td>
-                    <td>{{$booking->time}}</td>
-                    <td>{{$booking->size}}</td>
-                    <td>{{$booking->status}}</td>
-                    <td>
-                        @if($booking->status == 'Active')
-                        <form method="POST" action="/bookings/cancel/{{$booking->id}}">
-                        @csrf
-                        @method('PUT')
-                            <button>
-                                Cancel
-                            </button>
-                        </form>
-                        @endif
-                    </td>
-                    <td>
-                        @if($booking->status == 'Active')
-                        <form method="POST" action="/bookings/complete/{{$booking->id}}">
-                        @csrf
-                        @method('PUT')
-                            <button>
-                                Complete
-                            </button>
-                        </form>
-                        @endif
-                    </td>
-                </tr>
-                @endforeach
-                @else
-                <tr>
-                    <td>
-                        <p>No Bookings Found.</p>
-                    </td>
-                </tr>
-                @endunless
-            </tbody>
-        </table>
-    </div>
+<div class="mx-20 mt-14">
+    <h1 class="text-center text-4xl pb-8 text-black"> Booking List</h1>
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-sm text-black uppercase bg-gray-50 dark:bg-gray-700 dark:text-white">
+            <tr>
+                <td scope="col" class="py-7 px-14">
+                    Name
+                </td>
+                <td scope="col" class="py-7 px-14">
+                    Date
+                </td>
+                <td scope="col" class="py-7 px-14">
+                    Time
+                </td>
+                <td scope="col" class="py-7 px-14">
+                    Size
+                </td>
+                <td scope="col" class="py-7 px-14">
+                    Status
+                </td>
+                <td scope="col" class="py-7 px-14">
+                    Cancel
+                </td>
+                <td scope="col" class="py-7 px-14">
+                    Complete
+                </td>
+            </tr>
+        </thead>
+        <tbody>
+            @unless($bookings->isEmpty())
+            @foreach($bookings as $booking)
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <td class="py-7 px-14 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{$booking->user()->first()->name}}</td>
+                <td class="py-7 px-14">{{$booking->date}}</td>
+                <td class="py-7 px-14">{{$booking->time}}</td>
+                <td class="py-7 px-14">{{$booking->size}}</td>
+                <td class="py-7 px-14">{{$booking->status}}</td>
+                <td class="py-7 px-14">
+                    @if($booking->status == 'Active')
+                    <form method="POST" action="/bookings/cancel/{{$booking->id}}">
+                    @csrf
+                    @method('PUT')
+                        <button>
+                            Cancel
+                        </button>
+                    </form>
+                    @endif
+                </td>
+                <td class="py-7 px-14">
+                    @if($booking->status == 'Active')
+                    <form method="POST" action="/bookings/complete/{{$booking->id}}">
+                    @csrf
+                    @method('PUT')
+                        <button>
+                            Complete
+                        </button>
+                    </form>
+                    @endif
+                </td>
+            </tr>
+            @endforeach
+            @else
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <td class="py-7 px-14">
+                    <p>No Bookings Found.</p>
+            </td>
+        </tr>
+        @endunless
+        </tbody>
+    </table>
 </div> 
 @stop
