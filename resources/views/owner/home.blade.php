428d84ae373f96ea7ff956438cafc9739e390918
SokvisalMong <smong@paragoniu.edu.kh>
2022-10-21 12:43:22 +0700

    changed the top margin of admin home page and added a link to owner home page.

--- resources/views/owner/home.blade.php
+++ resources/views/owner/home.blade.php
@@ -6,6 +6,10 @@
 
 @section('content')
 <div class="text-center text-bold text-7xl my-80 w-full">
-    <h1 class="mr-28">Welcome to Owner page.</h1>
-</div>
+    <h1 class="mr-28">
+        Welcome to Owner page. To register your restaurant on our website, Click
+        <a href="/register" class="text-blue-400">Sign Up</a>
+        .
+    </h1>
+    </div>
 @stop
