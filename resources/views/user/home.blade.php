9d1d3d8265b47e02cb0ce383da37f29daaa449b8
Sivtheng <sivtheng25@gmail.com>
2022-10-21 12:46:52 +0700

    added link to owner domain in user home page

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <div>
     @include('layouts.user.hero')
-    <div class="mx-auto flex justify-center pt-32 pb-48">
+    <div class="mx-auto flex justify-center pt-32">
         <div class=" rounded overflow-hidden shadow-lg">
             <div class="px-6 py-6">
               <div class="font-bold text-xl mb-2 text-center">Restauranteurs Join Us Now</div>
@@ -23,5 +23,19 @@
             </div>
         </div>
     </div>
+    <div class="mx-auto flex justify-center pt-24 pb-20">
+      <div class=" rounded overflow-hidden shadow-lg">
+          <div class="px-6 py-6">
+            <div class="font-bold text-xl mb-2 text-center">Manage your restaurant here</div>
+            <p class="text-gray-700 text-base py-1">
+              Make edits to your restaurant, manage customer bookings, etc.
+            </p>
+            <p class="text-gray-700 text-base text-center py-1">
+              <a href="http://owner.localhost/" type="button" class="bg-blue-500 hover:bg-blue-700 text-white py-4 px-8 rounded-lg">Manage</a>
+            </p>
+          </div>
+      </div>
+  </div>
+
 </div>
 @stop
